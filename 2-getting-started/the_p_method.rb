b52512531c593706da7bcc943b76d83b8e351d07
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-14 22:12:35 +0100

    Add p method

---
+++ 2-getting-started/the_p_method.rb
@@ -0,0 +1,9 @@
+puts "Emmanuel Ogah"
+p "Emmanuel Ogah"
+
+
+puts "Hi there, this is a big line break
+I need a few minutes refreshment"
+
+p "Hi there, this is a big line break
+I need a few minutes refreshment"

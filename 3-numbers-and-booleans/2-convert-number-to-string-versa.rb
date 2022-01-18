a6feab5493ba8406ad8d77d72d67e2c59e604ce2
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-18 23:39:19 +0100

    conversion of string to number and float

---
+++ 3-numbers-and-booleans/2-convert-number-to-string-versa.rb
@@ -0,0 +1,30 @@
+str = "5"
+
+p str
+p str.class
+
+p str.to_i # convert string to integer
+p str.to_i.class 
+
+p str.to_f # convert string to float
+p str.to_f.class 
+
+number = 10
+
+p number
+
+p number.class
+
+p number.to_s # convert number to string
+p number.to_s.class
+
+p number.to_f # convert number to float
+p number.to_f.class
+
+pi = 3.14
+
+p pi.to_s # convert float to string
+p pi.to_s.class
+
+p pi.to_i # convert float to integer
+p pi.to_i.class

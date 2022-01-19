dd36e9cbc1615b496fbd5a2330168a05b39af730
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-19 20:32:53 +0100

    comparision operators

---
+++ 3-numbers-and-booleans/5-comparision-operator.rb
@@ -0,0 +1,16 @@
+# = is used to assign value to variable
+
+p 10 == 10 # compare the value at right and left hand side if they are equal. The value and data type must be the same
+p 10 == 20 # false
+
+p a = 10
+p b = 5
+p c = 10
+
+p a == c # true
+p b == c #false
+
+p "5" == 5 #false
+p "5" == "5" #true
+
+# != not equal to

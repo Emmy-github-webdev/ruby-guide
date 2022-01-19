3a680021c394a5c9fe77bea789a81db0698f652b
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-19 23:50:41 +0100

    downto and upto methods

---
+++ 3-numbers-and-booleans/downto-upto-methods.rb
@@ -0,0 +1,13 @@
+5.downto(1) {|i| puts "Countdown: #{i}"} # downto begins countdown from 5 to 1
+
+# Alternatively
+
+5.downto(1) do |number|
+  puts "We are in number #{number}"  
+end
+
+puts
+
+5.upto(10) do |number|
+  puts "We are in number #{number}"  
+end

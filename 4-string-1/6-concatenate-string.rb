9e071123c635eeb6f5ddea5588f58075a0b62ac8
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 15:43:12 +0100

    string concatenate

---
+++ 4-string-1/6-concatenate-string.rb
@@ -0,0 +1,16 @@
+first_name = "Emmanuel "
+firstname = "Emmanuel"
+last_name = "Ogah"
+lastname = "Ogah "
+
+#p first_name + last_name
+p first_name + last_name + " is a fullstack web developer"
+
+#p first_name += last_name
+#p first_name.concat(last_name) 
+p firstname.prepend(lastname)
+
+#suble operator <<
+
+#p first_name << last_name
+p first_name << last_name << " is a fullstack web developer"

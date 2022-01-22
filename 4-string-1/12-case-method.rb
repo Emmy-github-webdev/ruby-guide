f030596295d382b9c2f4c40f37e207fd05b2e237
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 17:45:06 +0100

    case method

---
+++ 4-string-1/12-case-method.rb
@@ -0,0 +1,15 @@
+#capitalize makes the first character in the string capital letter
+puts "hello".capitalize
+puts "hello world".capitalize
+
+#upcase turn all the letters to upper case, downcase turn all the letter to lower case
+
+puts "hello".upcase
+puts "hello world".upcase
+
+puts "HELLO".downcase
+puts "HELLO WORLD".downcase
+
+#swapcase swap the upper case and lower case respectively
+
+puts "EMMAnuel".swapcase

04eb7e7c543e4dde0067dee056f9c76cd015a049
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 17:12:39 +0100

    extract multiple  characters from string with range objects

---
+++ 4-string-1/9-extract-multiple-character-from-string-with-range-objects.rb
@@ -0,0 +1,8 @@
+story = "Once upon a time in a land far, far away..."
+p story[22..40]
+p story[22...40]
+
+p story.slice(22..40)
+p story.slice(22...40)
+
+# The difference between double and triple dots is that double dots includes the start and the end. In this case 22 and 40. However, triple dots excludes the start and the end

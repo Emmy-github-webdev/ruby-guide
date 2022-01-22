03c09a6c4e259fd201ed5b350051427a43e7dd63
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 14:16:16 +0100

    equality and inequality

---
+++ 4-string-1/5-equality-inequality-operators-with-string.rb
@@ -0,0 +1,13 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+puts a == b
+puts a == c
+puts c == b
+puts a != b
+
+#capital letters come before small leters
+
+p "A" < "a" # true
+p "Z" < "a" # true because, it will finish the capital letters before small letters

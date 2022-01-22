2f666b2d33bf1ba28b82396bec11f6fd290d4f48
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 16:47:11 +0100

    extract a single character from string using sqare and slice methods

---
+++ 4-string-1/8-extract-single-character-from-string.rb
@@ -0,0 +1,11 @@
+story = "Once upon a time in a land far, far away"
+
+#sqaure bracket
+p story[2] #extract the character in 2rd position
+p story[-1] #extract the character in the last position
+
+#slice method
+
+p story.slice(2)
+
+#Both slice and sqaure methods are the same

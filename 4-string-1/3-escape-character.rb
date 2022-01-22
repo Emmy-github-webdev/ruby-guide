aaf914d6056fb55f1346bcb31af4267711f5fcf2
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-22 13:48:11 +0100

    insert escape character

---
+++ 4-string-1/3-escape-character.rb
@@ -0,0 +1,11 @@
+#insert escape key
+puts "Julia said \"GoodBye\" to Remeo"
+puts 'Julia said \'GoodBye\' to Remeo'
+
+#insert line break
+result = "Let a add a line break\nright here"
+puts result
+
+#insert tab
+
+puts "\tInsert a tab right here"

74d1a3fa35d73f61a7b53dec7c9940c4ef07ad6f
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-25 00:45:12 +0100

    Parameters and argument

---
+++ 5-methos-and-conditionals-1/3-method-parameters-arguments.rb
@@ -0,0 +1,13 @@
+def praise_person(name)
+  puts "#{name} is talented!"
+end
+
+praise_person("Emmanuel")
+
+# multiple arguments
+
+def person(name, age)
+  puts "#{name} is #{age} years old."
+end
+
+person("Emmanuel", "70")

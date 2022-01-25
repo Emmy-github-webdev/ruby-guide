4f81beb99659e8de295bf200438e784f54e7fe8b
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-25 21:14:54 +0100

    Else if statement

---
+++ 5-methos-and-conditionals-1/8-if-else.rb
@@ -0,0 +1,20 @@
+color = "Green"
+
+if color == "Red"
+  puts "Red is rad"
+elsif color == "Green"
+  puts "Green is great!"
+end
+
+puts
+
+number = 55
+
+if number <= 25
+  puts "The number is low"
+elsif number < 50
+  puts "The number in the middle"
+elsif number <=70
+  puts "The number is great!"
+  
+end

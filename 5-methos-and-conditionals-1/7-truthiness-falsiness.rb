972b1782230e0ea6878d18e2611e9921f300c8ef
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-25 21:00:59 +0100

    Truthiness and falsiness

---
+++ 5-methos-and-conditionals-1/7-truthiness-falsiness.rb
@@ -0,0 +1,10 @@
+if nil
+  puts "This will be false"
+  # nil and false object type will be false
+end
+
+if 5
+  puts "this will be true"
+  #more truthiness are ahead
+  # Any other object type aside nil and false with be truthy
+end

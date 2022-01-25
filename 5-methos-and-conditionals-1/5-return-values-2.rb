d797ba56d8a01816f63bb9587bfb5607a20732d9
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-25 20:17:15 +0100

    Returns value 2

---
+++ 5-methos-and-conditionals-1/5-return-values-2.rb
@@ -0,0 +1,23 @@
+def nothing
+  
+end
+
+p nothing #This will return nil meaning nothing to return
+
+
+
+def return_string
+  "What is going to be return here?"
+end
+
+p return_string
+
+puts
+
+def return_guess
+  # puts, print here will always be evaluated to nil
+  puts "This will return a nil"
+  print "The print will also return nill"
+end
+
+p return_guess

d3fc13a83d37f3a343000a2bd0532f8aeae4a96c
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-25 23:15:27 +0100

    Multiple conditions

---
+++ 5-methos-and-conditionals-1/10-multiple-condition.rb
@@ -0,0 +1,9 @@
+status = "interviewed"
+stack = "Ruby on Rails"
+id = true
+
+if status == "applied" && stack = "Ruby on Rails"
+  puts "Congratulations! You are employed"
+elsif 
+  puts "You are invited for an interview with us"  
+end

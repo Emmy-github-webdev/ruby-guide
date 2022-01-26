86f9b2cf6e697d1c3880fbdfbd4ff948311b6f5a
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-27 00:22:42 +0100

    Unless keyword

---
+++ 6-methods-and-conditional-2/3-unless_keyword.rb
@@ -0,0 +1,15 @@
+password = "password"
+
+if password != "password"
+  puts "wrong paasword"
+else 
+  puts "Welcome"
+end
+
+# using unless
+
+unless password == "password"
+  puts "Wrong password"
+  else 
+    puts "Login successful"
+end

7ca2499f2a8a38512da25f69bede9cb789aeccbf
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-27 14:57:24 +0100

    Intro to classes

---
+++ 18-classes1/1-intro-to-class.rb
@@ -0,0 +1,7 @@
+# class is a blue print of creating an object
+# Object is an instance created from a class
+# Object Attributes describe information about the object
+# Variables are attributes that belongd to the object
+# Encapsulation is a concept that restricts direct access to an object
+# Instantination is the process of creating an object from a class
+

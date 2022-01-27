390ae7df5c90ee45acb6b4b5f1c5e2fc29b47843
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-27 16:01:46 +0100

    Intro to classes

--- 18-classes1/1-intro-to-class.rb
+++ 18-classes1/1-intro-to-class.rb
@@ -4,4 +4,5 @@
 # Variables are attributes that belongd to the object
 # Encapsulation is a concept that restricts direct access to an object
 # Instantination is the process of creating an object from a class
-
+# A superclass method is the class that a current class inherits from.
+# The class that inherit from the superclass is called the subclas.

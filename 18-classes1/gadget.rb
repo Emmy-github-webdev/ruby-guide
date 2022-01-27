2c0394cea9f768d55726b1c9d222498c0375a609
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-27 23:33:48 +0100

    classes

--- 18-classes1/gadget.rb
+++ 18-classes1/gadget.rb
@@ -1,11 +1,7 @@
 class Gadget
-  
+  def initialize
+    @username = "User #{ran(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
 end
-
-phone = Gadget.new
-laptop = Gadget.new
-microwave = Gadget.new
-
-puts phone
-puts laptop
-puts microwave.methods.sort

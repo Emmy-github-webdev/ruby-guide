75dad490c996fb9f9b3df057e71714fafb1d2e39
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 10:47:53 +0100

    Create an array

---
+++ 8-arrays/2-create-array.rb
@@ -0,0 +1,30 @@
+#Array of numbers
+number = [2, 4, 6, 8, 10, 12, 14, 16]
+puts number
+p number
+print number
+
+puts
+# Array of string
+
+toys = ["Teddy Bear", "Super Soaker", "board Game"]
+p toys
+
+puts
+
+# Array of different data type
+
+things = [1, "Hello", 10.2 ]
+p things
+
+puts
+
+
+# Multi dimensional array
+students = [
+  ["Emmanuel", 35, true],
+  ["Mary", 10, true],
+  ["Doe", 15],
+]
+
+p students

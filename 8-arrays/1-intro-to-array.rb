d22b44116de045b1689b614aec6459c6f290fe17
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 10:28:43 +0100

    Intro to array

---
+++ 8-arrays/1-intro-to-array.rb
@@ -0,0 +1,6 @@
+# An array is the ordered collection of object
+# It is created with a pair of square bracket
+
+p [] #print []
+p [].class # print class of the array
+p number = [] # Assign variable to an array

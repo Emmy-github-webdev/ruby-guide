1a2449034a73f276b97cb5703e6be16a14905663
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 23:19:10 +0100

    each with method

--- 8-arrays/11-each-method.rb
+++ 8-arrays/11-each-method.rb
@@ -8,3 +8,44 @@ end
   square = num * num
   puts "The squre of #{num} is #{square}"
 end
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+odd = []
+
+numbers.each do |number|
+  if number.odd?
+    odd.push(number)
+  end
+end
+
+p odd
+
+
+num_Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+evens = 0
+odds = 0
+
+num_Array.each do |sum_array|
+  if sum_array.even?
+    evens += sum_array
+  elsif sum_array.odd?
+    odds += sum_array
+  else
+    puts sum_array 
+  end
+end
+
+p evens
+p odds
+
+#using method
+def add_odds_or_evens(arrays)
+  odds = []
+  evens = []
+  arrays.each { |array| array.even? ? evens << array : odds << array } 
+  p odds
+  p evens
+end
+
+add_odds_or_evens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])

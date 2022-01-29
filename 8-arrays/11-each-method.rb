f3f373a1ef2b9a05cf0129c7fff176c2cd131938
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 22:33:15 +0100

    Block and each method

---
+++ 8-arrays/11-each-method.rb
@@ -0,0 +1,10 @@
+fruits = ["Apple", "Orange", "Pineapple", "Banana", "Pear"]
+
+fruits.each do |fruit|
+  puts fruit.upcase
+end
+
+[1, 2, 3, 4, 5].each do |num|
+  square = num * num
+  puts "The squre of #{num} is #{square}"
+end

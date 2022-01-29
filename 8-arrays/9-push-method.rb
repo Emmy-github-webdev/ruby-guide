82a0fca56e1eb083a0e8652b2dd6279820248d03
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 17:22:44 +0100

    push method

---
+++ 8-arrays/9-push-method.rb
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Orange", "Pineapple", "Banana", "Pear"]
+p fruits
+fruits.push("Watermelon")
+p fruits
+fruits << "Pip" << "pap" #suble method
+p fruits
+fruits.insert(1, "bobo") # 1 is the position the item will be inserted
+p fruits

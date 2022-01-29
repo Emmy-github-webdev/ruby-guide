17e15d565e49ad91d44a78e29ddf5dbeaea211cf
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 17:36:24 +0100

    shift and unshift method

--- 8-arrays/10-pop-method.rb
+++ 8-arrays/10-pop-method.rb
@@ -2,3 +2,8 @@ fruits = ["Apple", "Orange", "Pineapple", "Banana", "Pear"]
 p fruits
 fruits.pop
 p fruits
+
+#pop remove item from the end
+# shift remove from the beginning
+fruits.shift
+p fruits
--- 8-arrays/9-push-method.rb
+++ 8-arrays/9-push-method.rb
@@ -6,3 +6,6 @@ fruits << "Pip" << "pap" #suble method
 p fruits
 fruits.insert(1, "bobo") # 1 is the position the item will be inserted
 p fruits
+
+# push add item to the end of the array
+#unshift add item to the beginning of the array

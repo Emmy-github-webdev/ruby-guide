a93cdbb87dd30af0278619ff579b2edc99fab1cd
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-29 11:31:22 +0100

    Array index position

---
+++ 8-arrays/4-array.new-method.rb
@@ -0,0 +1,3 @@
+p Array.new(10, [true]) # the first value 10 specify the number of times the second value will be print
+p Array.new(5, "Hello")
+p Array.new(7, [2, "world", 0.2])
---
+++ 8-arrays/5-array-index-position.rb
@@ -0,0 +1,10 @@
+fruits = ["Apple", "Orange", "Pineapple", "Banana", "Pear"]
+
+p fruits.length
+p fruits[0]
+p fruits[3]
+
+# get the last item in the array
+p fruits[fruits.length - 1]
+p fruits[-1]
+p fruits.[](-1)

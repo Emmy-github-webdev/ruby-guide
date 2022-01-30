f98c6f21b66b3872dc7373f51d456f0407d150ec
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-30 16:48:58 +0100

    Modules with methods

---
+++ 19-modules/1-intro-t--module.rb
@@ -0,0 +1,24 @@
+# A module is a toolbox or container of methods and constanats.
+
+module Square
+  def self.area(side)
+    side * side
+  end
+end
+
+module Rectangle 
+  def self.area(length, width)
+    length * width
+  end
+end
+
+module Circle
+  PI = 3.14159
+  def self.area(radius)
+    PI * radius * radius
+  end
+end
+
+puts Square.area(10)
+puts Rectangle.area(5, 13)
+puts Circle.area(7)

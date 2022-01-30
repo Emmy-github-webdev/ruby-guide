f652ea66db6b236a6d6c04205c3f476394297252
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-30 16:17:57 +0100

    inatance variable

--- 18-classes1/gadget.rb
+++ 18-classes1/gadget.rb
@@ -1,7 +1,12 @@
 class Gadget
   def initialize
-    @username = "User #{ran(1..100)}"
+    @username = "User #{rand(1..100)}"
     @password = "topsecret"
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 end
+
+phone = Gadget.new
+laptop = Gadget.new
+p phone.instance_variables
+p laptop.instance_variables

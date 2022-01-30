b17b39fe7ad28d48e92fb35ddc6b03b29b1681c1
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-30 20:50:06 +0100

    subclass

---
+++ classes/subclass.rb
@@ -0,0 +1,16 @@
+class Employee
+  attr_accessor :age
+  attr_reader :name
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def introduce
+    "Hi, my name is #{name} and I am #{age} years old"
+  end
+end
+
+emma = Employee.new("Emmanuel", 30)
+puts emma

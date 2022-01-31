fd6a49333a48ee397136bd9122d483e2c8a4e15f
Emmanuel Oga <ogahemmanuel75@gmail.com>
2022-01-31 09:35:38 +0100

    Update student class

--- oop/student.rb
+++ oop/student.rb
@@ -1,14 +1,21 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
 
   def to_s
-    "First name is #{@first_name}"
+    "I am #{@first_name} #{last_name} with email address #{email}. My credentials includes #{username} and #{password}"
   end
 end
 
-student1 = Student.new
+student1 = Student.new("Emmanuel", "Ogah", "Emmanuel.ogah@example.com", "Emmanuel1", "123")
+student2 = Student.new("John", "Doe", "john.doe@example.com", "john1", "123")
 puts student1
+puts
+puts student2

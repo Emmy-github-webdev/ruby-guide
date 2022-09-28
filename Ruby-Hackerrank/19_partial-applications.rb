beb5e34882d67011962ba208184623fdc32b94c5
emmanuel <ogahemmanuel75@gmail.com>
2022-09-28 12:51:20 +0000

    Partial application

--- Ruby-Hackerrank/19_partial-applications.rb
+++ Ruby-Hackerrank/19_partial-applications.rb
@@ -1,13 +1,11 @@
-combination = -> (n) do
-  -> (r) do
-      numerator = Math.gamma(n + 1)
-      r_number = Math.gamma(r + 1)
-      r_n_number = Math.gamma(r_number + 1)
-      denuminator = r_number * r_n_number
-      (numerator / denuminator).to_i
-  end
+-> (another_number) do
+  numerator = Math.gamma(number + 1)
+  fact_another_number = Math.gamma(another_number + 1)
+  fact_both_numbers = Math.gamma((number-another_number)+1)
+  denominator = fact_another_number*fact_both_numbers
+  (numerator / denominator).to_i
 end
-
+end
 n = gets.to_i
 r = gets.to_i
 nCr = combination.(n)

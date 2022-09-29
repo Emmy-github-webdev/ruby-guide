7aafa0fe573533d63eb26f1f90247e00efd8e8d6
emmanuel <ogahemmanuel75@gmail.com>
2022-09-29 14:19:11 +0000

    Lazy evaluation

--- Ruby-Hackerrank/21_lazy-evaluation.rb
+++ Ruby-Hackerrank/21_lazy-evaluation.rb
@@ -1,11 +1,8 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-prime_number = -> (num) do
-  (2.upto(num.to_i).select {|n| num.to_i % n.to_i == 0}).size == 1 ? true : false
+require 'prime'
+print_prime_number = -> (n) do
+    Prime.lazy.select {|x| x if x.to_s == x.to_s.reverse}.first(n)
 end
 
-print_array = -> (n) do
-1.upto(Float::INFINITY).lazy.select {|a| (a.to_s == a.to_s.reverse && prime_number.(a)) ? a : false }.first(n.to_i)
-end
-
-n = gets
-print print_array.(n)
+n = gets.to_i
+print print_prime_number.(n)
